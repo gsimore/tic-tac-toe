bdcce43baecc16b29b15ffae19ee42e703a57224
Gabrielle Simard-Moore <gsimardmoore@cloudbolt.io>
2018-01-10 16:49:37 -0800

    Add history and a restart game button. Restart works by using a predefined initial state for the Game and using setState to the initial onClick.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -12,52 +12,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  // constructor for the board with an array of empty (null) squares.
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-        xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-      // call .slice() to copy squares instead of mutating the existing array.
-      const squares = this.state.squares.slice();
-      // check if someone has won; if so, return the winner.
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      // check which player's turn it is; X or O
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        // toggle boolean
-        xIsNext: !this.state.xIsNext,
-      });
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-        status = 'Winner: ' + winner;
-    } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -77,17 +43,94 @@ class Board extends React.Component {
     );
   }
 }
+const initialGameState = {
+  history: [{
+    squares: Array(9).fill(null)
+  }],
+    stepNumber: 0,
+    xIsNext: true,
+};
 
 class Game extends React.Component {
+  // the initial state of the game.
+  constructor(props) {
+    super(props);
+    this.state = initialGameState
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    // call .slice() to copy squares instead of mutating the existing array.
+    const squares = current.squares.slice();
+    // check if someone has won; if so, return the winner
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    // check which player's turn it is; X or O
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,   // toggle boolean
+    });
+  }
+
+  restart() {
+    this.setState()
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      );
+    });
+
+    let status;
+    let start;
+    if (winner) {
+      status = 'Winner: ' + winner;
+      start = 'Rematch!';
+    } else {
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+      start = 'Restart Game'
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <button onClick={() => this.setState(initialGameState)}>{start}</button>
+          <div>{status}</div>
+          <ol>
+            {moves}
+          </ol>
         </div>
       </div>
     );
